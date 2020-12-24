d3d99e6a939a4299830210c700de079ef51f3762
Kallum Tolkien <kallumtolkien@gmail.com>
2020-12-24 12:53:04 +0000

    objects kata's completed

--- src/objects.js
+++ src/objects.js
@@ -1,44 +1,52 @@
 const createPerson = (name, age) => {
   return {
-    name: name,
-    age: age
+    name,
+    age
   };
 };
 
 const getName = object => {
-  // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object[property];
 };
 
 const hasProperty = (property, object) => {
-  // your code here
+  return object.hasOwnProperty(property);
 };
 
 const isOver65 = person => {
-  // your code here
+  return person.age > 65;
 };
 
 const getAges = people => {
-  // your code here
+  return people.map(person => person.age);
 };
 
 const findByName = (name, people) => {
-  // your code here
+  return people.find(person => person.name == name);
 };
 
 const findHondas = cars => {
-  // your code here
+  return cars.filter(car => car.manufacturer == 'Honda');
 };
 
 const averageAge = people => {
-  // your code here
+  const ages = people.map(person => person.age);
+  const totalAges = ages.reduce((accumulator, currentVal) => accumulator + currentVal);
+  return totalAges / 3;
 };
 
 const createTalkingPerson = (name, age) => {
-  // your code here
+  return {
+    name,
+    age,
+    introduce() {
+      return `Hi Fred, my name is ${name} and I am ${age}!`;
+    }
+  };
 };
 
 module.exports = {
